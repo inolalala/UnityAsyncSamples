d23fe450da43196a20c8c272f2ab87e350678bce
inolalala <inoue-sota@kayac.com>
2020-07-08 21:28:38 +0900

    ラムダ式版追記

--- Project/Assets/SimpleTaskMain.cs
+++ Project/Assets/SimpleTaskMain.cs
@@ -10,6 +10,16 @@ public class SimpleTaskMain : MonoBehaviour
     async void Start()
     {
         Task<int> task = Task.Run<int>(new Func<int>(sluggishCalculate));
+        //ラムダ式でもOK
+        // Task<int> task = Task.Run<int>(() => {
+        //     int retval=0;
+        //     for(int i=0; i<100; i++)
+        //     {
+        //         retval+=i;
+        //     }
+        //     Thread.Sleep(5000);//重たい処理
+        //     return retval;
+        // });
         int result = await task;
         Debug.Log(result);
     }
